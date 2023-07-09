66916948b7f50ff389b8bb40d674997fac11870d
Marisaguty <marisaguty@outlook.com>
2023-07-09 18:28:17 +0200

    login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,8 +1,39 @@
-
+import { MainPage } from "../page-object/main"
 
 describe('Test login', () => {
-  it('Login correcto', () => {
-    cy.visit('https://www.safeguru.com')
+  const mainPage = new MainPage();
+  beforeEach(()=>{
+    mainPage.navegarPaginaPrincipal()
   })
+  /* it('Crear cuenta', () => {
+    mainPage.clickCuenta()
+    mainPage.clickRegistrar()
+    mainPage.rellenarNombre('Marisa')
+    mainPage.rellenarApellido('Gutierrez')
+    mainPage.rellenarEmail('mgutyper@gmail.com')
+    mainPage.rellenarContraseña('12345678mg')
+    mainPage.clickPoliticaPrivacidad()
+    mainPage.clickContinuar()
+  }) */
+  /* it('Login correcto y añadir telefono', () =>{
+    mainPage.clickCuenta()
+    mainPage.rellenarLoginEmail('mgutyper@gmail.com')
+    mainPage.rellenarContraseña('12345678mg')
+    mainPage.clickIniciarSesion()
+    // mainPage.cerrarSesion('Cerrar sesión') 
+    // mainPage.añadirTelefono('666666666') 
+  }) */
+
+  it('Login contraseña incorrecta y cambiar contraseña', ()=>{
+    mainPage.clickCuenta()
+    mainPage.rellenarLoginEmail('mgutyper@gmail.com')
+    mainPage.rellenarLoginContraseñaIncorrecta('jdahdadiahd')
+    mainPage.clickIniciarSesion()
+    mainPage.clickOlvidarContraseña()
+    mainPage.rellenarRecuperarContraseña('mgutyper@gmail.com')
+    mainPage.clickEnviarEmail()
+  })
+
+  
   
 })
--- cypress/page-object/main.js
+++ cypress/page-object/main.js
@@ -12,5 +12,60 @@ export class MainPage{
     comprobarResultado(text){
         cy.get('.text-wrapper').contains(text)
     }
-    
+    clickCuenta(){
+        cy.get('.nav-links > :nth-child(3) > a').click()
+        
+    }
+    clickRegistrar(){
+        cy.get('.sc-erijoS').click()
+    }
+    rellenarNombre(name){
+        cy.get('.sc-dDPqvT > :nth-child(1) > .sc-jTjUTQ').type(name)
+    }
+    rellenarApellido(lastName){
+        cy.get('.sc-dDPqvT > :nth-child(2) > .sc-jTjUTQ').type(lastName)
+    }
+    rellenarEmail(email){
+        cy.get(':nth-child(3) > .sc-jTjUTQ').type(email)
+    }
+    rellenarContraseña(pass){
+        cy.get('.sc-hixjlP > .ehrPVE > .sc-jTjUTQ').type(pass)
+    }
+    clickPoliticaPrivacidad(){
+        cy.get('.sc-cbfGDZ > .sc-GKYbw > .sc-eGugkK > .sc-csDkEv').click()
+    }
+    clickContinuar(){
+        cy.get('.sc-dDPqvT > .sc-lllmON').click()
+    }
+    rellenarLoginEmail(email){
+        cy.get('.sc-hixjlP > .iAqfve > .sc-jTjUTQ').type(email)
+    }
+    rellenarLoginContraseña(pass){
+        cy.get('.sc-hixjlP > .ehrPVE > .sc-jTjUTQ').type(pass)
+    }
+    clickIniciarSesion(){
+        cy.get('.sc-hixjlP > .sc-lllmON > div').click();
+    }
+   /*  cerrarSesion(text){
+        cy.get('.nav-links > :nth-child(3) > button').click()
+        cy.get('.sc-a65312e0-2 > .sc-a65312e0-0 > .sidebar > .options-menu > :nth-child(5) > .option-menu').contains(text).click()
+    } */ /* no me funciona */
+    /* añadirTelefono(number){
+        cy.get('.nav-links > :nth-child(3) > button').click()
+        cy.get(':nth-child(3) > .sc-jTjUTQ').type(number)
+    } */ /* no me  funciona */
+
+    rellenarLoginContraseñaIncorrecta(pass){
+        cy.get('.sc-hixjlP > .ehrPVE > .sc-jTjUTQ').type(pass)
+    }
+    clickOlvidarContraseña(){
+        cy.get(':nth-child(4) > .sc-eZceyY').click()
+    }
+    rellenarRecuperarContraseña(email){
+        cy.get('.inputContainer > .sc-bTTELM > .sc-jTjUTQ').type(email)
+    }
+    clickEnviarEmail(){
+        cy.get('.sc-ddKZzx > .buttons > :nth-child(1)').click()
+        cy.get('.sc-dYBPgv > .sc-lllmON').click()
+    }
 }
